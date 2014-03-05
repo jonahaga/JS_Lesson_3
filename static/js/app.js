c87036f81098da2aa1602f71f7e950be93d72af2
Hackbright Student <info@hackbrightacademy.com>
2014-03-04 18:07:29 -0800

    Static JS files

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,44 @@
 $(document).ready(function(){
+    var formSubmitButton = $("#form_submit");
+    var itemFormSubmit = $("#item_form_submit");
+
+    // To-Do Lists Form
+    formSubmitButton.on("click", function(event){
+        event.preventDefault(); // prevent the browser form submission from happening
+        $.ajax({
+            url: "/",
+            method: "POST",
+            data: $("form#todo_list_form").serialize(),
+        }).done(function(data){
+            $('#todo-list')[0].innerHTML = data;
+        }).fail(function(){
+            // console.log('fail!!!');
+        });
+    });
+
+    // Gets updated to-do list from server and changes HTML
+    var getListsFromServer = function() {
+        $.ajax({
+            url:'/todo_lists/poll',
+            method: "GET"
+        }).done(function(data){
+            $('#todo-list').html(data);
+        });
+    };
+
+    // To-Do Items Form
+    itemFormSubmit.on("click", function(event){
+        event.preventDefault();
+        $.ajax({
+            url: window.location.pathname,
+            method: "POST",
+            data: $("form#todo-item-form").serialize(),
+        }).done(function(data){
+            $('#todo-list')[0].innerHTML = data;
+        }).fail(function(){
+
+        });
+    });
+
+
 });
